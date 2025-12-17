9e0e2fecd7f060ffbc9534c4e603f23ed41a5f47
manar <mnarzaid247@gmail.com>
2025-12-17 01:19:55 +0000

    creatint 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting an array
+ *
+ * @array: pointer to an array
+ * @size: size_t
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i;
+size_t j;
+int tmp;
+if (array == NULL)
+	return;
+if (size < 2)
+	return;
+for (i = 0 ; i < size ; i++)
+	for (j = 0 ; j < size - i - 1 ; j++)
+	{
+		if (array[j] > array[j + 1])
+			{
+			tmp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = tmp;
+			print_array(array, size);
+			}
+	}
+}
