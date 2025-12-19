ed72a38b0a502038601f1cf785ec54a389e499dc
Aljawharah <aljawharhs18@gmail.com>
2025-12-19 18:22:43 +0300

    refactor: streamline quick_sort and lomuto_partition functions for clarity and efficiency

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,21 +8,52 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot, j;
-	int i, temp;
-
 	if (array == NULL || size < 2)
 		return;
+	quick_sort_recursive(array, size, 0, size - 1);
+}
+/**
+* quick_sort_recursive - recursive function to perform quick sort
+* @array: pointer to the array to be sorted
+* @size: size of the array
+* @low: starting index of the sub-array
+* @high: ending index of the sub-array
+*/
+void quick_sort_recursive(int *array, size_t size, size_t low, size_t high)
+{
+	size_t pivot_index;
 
-	pivot = size - 1;
-	i = -1;
-
-	for (j = 0; j < size - 1; j++)
+	if (low < high)
 	{
-		if (array[j] <= array[pivot])
+		pivot_index = lomuto_partition(array, size, low, high);
+		if (pivot_index > 0)
+			quick_sort_recursive(array, size, low, pivot_index - 1);
+		quick_sort_recursive(array, size, pivot_index + 1, high);
+	}
+}
+/**
+* lomuto_partition - partitions the array using the Lomuto partition scheme
+* @array: pointer to the array to be partitioned
+* @size: size of the array
+* @low: starting index of the sub-array
+* @high: ending index of the sub-array
+* Return: index of the pivot element after partitioning
+*/
+int lomuto_partition(int *array, size_t size, size_t low, size_t high)
+{
+	int pivot = array[high];
+
+	size_t i = low - 1;
+	int temp;
+
+	size_t j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
 		{
 			i++;
-			if (i != (int)j)
+			if (i != j)
 			{
 				temp = array[i];
 				array[i] = array[j];
@@ -31,16 +62,12 @@ void quick_sort(int *array, size_t size)
 			}
 		}
 	}
-
-	if (i + 1 != (int)pivot)
+	if (i + 1 != high)
 	{
 		temp = array[i + 1];
-		array[i + 1] = array[pivot];
-		array[pivot] = temp;
+		array[i + 1] = array[high];
+		array[high] = temp;
 		print_array(array, size);
 	}
-	if (i + 1 > 0)
-		quick_sort(array, i + 1);
-	if (size - i - 2 > 0)
-		quick_sort(array + i + 2, size - i - 2);
+	return (i + 1);
 }
